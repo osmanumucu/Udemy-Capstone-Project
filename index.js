86570c05373c100b8d70c27d8998362f116868ac
Osman Talha Umucu <osmanumucu40@gmail.com>
2024-06-01 15:58:34 +0200

    edit blog succeeded

--- index.js
+++ index.js
@@ -6,10 +6,33 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let posts = [];
+
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", {posts});
+});
+
+app.get("/add", (req, res) => {
+    res.render("add.ejs");
+});
+
+app.post("/add", (req, res) => {
+    const {title, content} = req.body;
+    posts.push({title, content});
+    res.redirect("/");
+});
+
+app.get("/edit/:id", (req, res) => {
+    const post = posts[req.params.id];
+    res.render("edit.ejs", {post, id: req.params.id});
+})
+
+app.post("/edit/:id", (req, res) => {
+    const {title, content} = req.body;
+    posts[req.params.id] = {title, content};
+    res.redirect("/");
 })
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
